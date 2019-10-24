b495ffa0d35c077c71fe717165889165f3ae23aa
dinhtienn <dinhtiennguyen.1202@gmail.com>
2019-10-24 10:53:49 +0700

    Create Social seeder

---
+++ database/seeds/SocialTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Social;
+
+class SocialTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $list_social = [
+            [
+                'social' => 'Facebook',
+                'link' => 'https://facebook.com',
+            ],
+            [
+                'social' => 'Google',
+                'link' => 'https://google.com',
+            ],
+            [
+                'social' => 'Twitter',
+                'link' => 'https://twitter.com',
+            ],
+        ];
+        foreach ($list_social as $social) {
+            Social::create($social)->save();
+        }
+    }
+}
